e58d436b5c12c1364b910b3c3aa729ee8edf6959
Klaudia Synarska <klaudiasynarska@wp.pl>
2024-04-06 18:50:44 +0200

    fetch countries from API

--- app.js
+++ app.js
@@ -18,15 +18,36 @@ btn.addEventListener("click", function () {
     let loader = document.querySelector(".loader");
     loader.style.display = "inline";
 
-    setTimeout(function () {
-        let destination = destinations[getRandomNumber()];
-        lotteryResult.innerHTML = "<h2>" + destination["name"] + "</h2>";
-        lotteryResult.innerHTML += "<img width=200 src=" + destination["image_url"] + "></img>";
-        loader.style.display = "none";
-        btn.textContent = "Try again";
-    }, 2000);
+    // setTimeout(function () {
+    //     let destination = destinations[getRandomNumber()];
+    //     lotteryResult.innerHTML = "<h2>" + destination["name"] + "</h2>";
+    //     lotteryResult.innerHTML += "<img width=200 src=" + destination["image_url"] + "></img>";
+    //     loader.style.display = "none";
+    //     btn.textContent = "Try again";
+    // }, 2000);
+
+    fetch("https://api.sampleapis.com/countries/countries")
+        .then(response => {
+            console.log("fetched");
+            if (!response.ok) {
+                throw new Error("Fetch error");
+            }
+            return response.json();
+        })
+        .then(data => {
+            loader.style.display = "none";
+
+            let length = data.length;
+            let destination = data[getRandomNumber(length)]
+            lotteryResult.innerHTML = destination["name"];
+            lotteryResult.innerHTML += "<img width=200 src=" + destination["media"]["flag"] + "></img>";
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * destinations.length);
+function getRandomNumber(length) {
+    return Math.floor(Math.random() * length);
 }
